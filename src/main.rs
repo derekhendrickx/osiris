0265e89751f22c2f62d5afd1f97ae985c24125a4
Derek Hendrickx <derek@hendrickx.me>
2023-06-22 00:44:54 -0400

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod udp_server;
 
 use std::{
-    error::Error,
     net::{IpAddr, Ipv4Addr, SocketAddr},
 };
 
 use clap::Parser;
 use flexi_logger::Logger;
-use log::info;
+use log::{info, debug};
+use tokio::net::UdpSocket;
 
 /// Osiris torrent tracker
 #[derive(Parser, Debug)]
@@ -19,17 +19,23 @@ struct Args {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Logger::try_with_str("debug")?.start()?;
 
     info!("Osiris version 0.1 - Torrent tracker");
 
     let args = Args::parse();
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), args.port);
-    // let udp_tracker = UdpTracker::new(addr).await?;
-    // info!("Listening on: {}", udp_tracker.socket.local_addr()?);
+    let socket = UdpSocket::bind(addr).await?;
 
-    // udp_tracker.run().await?;
+    info!("Listening on {:?}:{:?}", addr.ip(), addr.port());
 
-    Ok(())
+    let mut buffer = [0; 1024];
+    loop {
+        let (len, addr) = socket.recv_from(&mut buffer).await?;
+        debug!("{:?} bytes received from {:?}", len, addr);
+
+        let len = socket.send_to(&buffer[..len], addr).await?;
+        debug!("{:?} bytes sent", len);
+    }
 }
